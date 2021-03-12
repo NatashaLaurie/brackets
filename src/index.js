a37306114e774641133dd294af44b3608aa51fd0
NatashaLaurie <71662387+NatashaLaurie@users.noreply.github.com>
2021-03-12 15:06:01 +0300

    init: add brackets-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+        if (str[i] === bracketsConfig[j][0]) {
+          if (stack.indexOf(bracketsConfig[j][0]) > -1 && bracketsConfig[j][0] === bracketsConfig[j][1]) {
+            stack.pop();
+            continue;
+          }
+          stack.push(str[i]);
+        } 
+        if (str[i] === bracketsConfig[j][1]) {
+          if (stack.length && stack[stack.length - 1] === bracketsConfig[j][0] && bracketsConfig[j][0] !== bracketsConfig[j][1]) {
+            stack.pop();
+          } else if (!stack.length && stack[stack.length - 1] !== bracketsConfig[j][0] && bracketsConfig[j][0] !== bracketsConfig[j][1]) {
+            return false;
+          }
+        } 
+    }
+  }
+  return stack.length === 0;
 }
